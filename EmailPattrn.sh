b947c7a6dfe39b6c354ef11e9966222acf1a5791
unknown <nksayyad2900.com>
2020-10-14 18:32:41 +0530

    Added: Email Validation...

---
+++ EmailPattrn.sh
@@ -0,0 +1,16 @@
+#!/bin/bash +x
+echo "-----Email Pattern-----"
+
+#valid Format: abc.xyz@bridgelabz.co.in
+#madatory part: abc, bridgelabz, co
+
+format='^[a-zA-Z0-9]+(.[a-zA-Z0-9])*@[a-zA-Z0-9]+.[a-z]{2,4}+(.[a-z]{2})*$'
+
+read -p "Enter Email Id: " email
+
+if [[ $email =~ $foramt ]]
+then
+	echo "$email   IS VALID EMAIL."
+else
+	echo "$email    is Invalid Email-Id ???"
+fi
