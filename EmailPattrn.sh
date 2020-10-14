9605f7639c05ef9af2412f9c20aa1d5f09a8afa1
unknown <nksayyad2900.com>
2020-10-14 19:41:17 +0530

    Adedd: .co Mandatory part

--- EmailPattrn.sh
+++ EmailPattrn.sh
@@ -3,9 +3,9 @@
 echo "-----Email Pattern-----"
 shopt -s extglob
 
-#valid Format: abc.xyz@bridgelabz.com 
+#valid Format: abc.xyz@bridgelabz.co
 
-format='^[a-zA-Z0-9]+[.(a-zA-Z0-9)]+(\@)[a-zA-Z0-9]+(\.)[a-z]{2,4}$'
+format='^[a-zA-Z0-9]+[.(a-zA-Z0-9)]*(\@)[a-zA-Z0-9]+(\.)([a-z]{2,4})+([.][a-z]{2})*$'
 
 read -p "Enter Email Id: " email
 
