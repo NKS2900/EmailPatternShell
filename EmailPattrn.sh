b3d574ee8c245771f04169c781ec28954d6b825e
NIJAM SAYYAD <58353284+NKS2900@users.noreply.github.com>
2020-10-15 00:12:53 +0530

    Added : TLD can starts with DOT

--- EmailPattrn.sh
+++ EmailPattrn.sh
@@ -5,7 +5,7 @@ shopt -s extglob
 
 #valid Format: abc.xyz@bridgelabz.com 
 
-format='^[a-zA-Z0-9]+[.(a-zA-Z0-9)]+(\@)[a-zA-Z0-9]+(\.)[a-z]{2,4}$'
+format='^[a-zA-Z0-9]+[-(a-zA-Z0-9)]+(\@)[a-zA-Z0-9]+(\.)[a-z]{2,4}$'
 
 read -p "Enter Email Id: " email
 
